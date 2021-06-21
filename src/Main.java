68ccac768b9d0b4c266d9f620080dcd3124b24da
Anders Säfsten <69458018+saefsten@users.noreply.github.com>
2021-06-21 13:42:22 +0200

    feature addNumbers

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("this is a line printed");
+        int a = 8;
+        int b = 4;
 
+        System.out.format("\n%d plus %d is %d\n", a, b, addNumbers(a, b));
+
+    }
+
+    public static int addNumbers(int a, int b) {
+        return a + b;
     }
 }
