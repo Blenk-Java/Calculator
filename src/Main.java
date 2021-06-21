75dfff5a53c413131c28e08892664d67ebb0d006
Philip Blenk <P.blenk@hotmail.com>
2021-06-21 13:42:39 +0200

    added message

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int x = 0;
+        int y = 0;
+
         System.out.println("this is a line printed");
+
+
+
+
+    }
+    public static int multiply(int x, int y){
 
     }
 }
